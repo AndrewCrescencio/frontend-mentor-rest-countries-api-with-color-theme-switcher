95e8473337b0fd950056d660ce094e4163d50f50
Andrew Crescencio <andrew_workstation@hotmail.com>
2022-11-17 05:14:10 -0300

    :lipstick: fix: AppNavbar css

--- components/App/Navbar/index.vue
+++ components/App/Navbar/index.vue
@@ -1,23 +1,27 @@
 <template>
   <nav
-    class="h-20 px-4 bg-white dark:bg-lightDarkBg flex items-center justify-between shadow-navbar lg:px-20 text-[#111517] dark:text-[#fff]"
+    class="bg-white dark:bg-lightDarkBg shadow-navbar lg:px-20 text-[#111517] dark:text-[#fff]"
   >
-    <h1 class="font-extrabold font-nunitoBold text-sm md:text-2xl">
-      Where in the world?
-    </h1>
-    <button
-      class="flex items-center p-0 gap-x-2 font-font-nunitoSemibold"
-      @click="changeColorMode"
-    >
-      <LazyIconMoon
-        v-if="$colorMode.value === 'light'"
-        class="w-4 h-4 md:w-5 md:h-5"
-      />
-      <LazyIconSun v-else class="w-4 h-4 md:w-5 md:h-5" />
-      <span class="text-xs md:text-base leading-none font-semibold"
-        >{{ $colorMode.value === 'light' ? 'Dark' : 'Light' }} Mode</span
+    <div class="container flex items-center justify-between h-20">
+      <h1
+        class="font-extrabold font-nunitoBold font-font-extrabold text-sm md:text-2xl"
       >
-    </button>
+        Where in the world?
+      </h1>
+      <button
+        class="flex items-center p-0 gap-x-2 font-nunitoSemibold"
+        @click="changeColorMode"
+      >
+        <LazyIconMoon
+          v-if="$colorMode.value === 'light'"
+          class="w-4 h-4 md:w-5 md:h-5"
+        />
+        <LazyIconSun v-else class="w-4 h-4 md:w-5 md:h-5" />
+        <span class="text-xs md:text-base leading-none font-semibold"
+          >{{ $colorMode.value === 'light' ? 'Dark' : 'Light' }} Mode</span
+        >
+      </button>
+    </div>
   </nav>
 </template>
 
