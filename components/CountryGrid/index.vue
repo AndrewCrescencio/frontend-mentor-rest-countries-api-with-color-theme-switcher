d216b0e5e1d685e717f5b7797b2d38d354ca5c3c
Andrew Crescencio dos Santos <68707868+AndrewCrescencio@users.noreply.github.com>
2022-12-30 17:52:35 -0300

    :bug: fix: ContryGrid component

--- components/CountryGrid/index.vue
+++ components/CountryGrid/index.vue
@@ -1,15 +1,13 @@
 <template>
   <section class="container">
-    <!--
-      class="mt-8 mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between gap-10 xl:gap-[74px]"
-      mt-8 flex flex-wrap justify-center gap-10 lg:(mt-12 gap-[74px])
-     -->
     <div
       class="mt-8 flex flex-wrap justify-center gap-10 lg:(mt-12 gap-y-[75px] gap-x-[74px])"
     >
-      <template v-for="(country, i) in content">
-        <CountryCard :key="i" :country="country" />
-      </template>
+      <CountryCard
+        v-for="(country, i) in content"
+        :key="i"
+        :country="country"
+      />
     </div>
   </section>
 </template>
